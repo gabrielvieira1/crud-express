d1df76159ccf0f8dfc57942abb8e8a8e33d75ae6
Gabriel Vieira <gabrielvieira1@protonmail.com>
2021-12-29 20:56:38 -0300

    CRUD Categories

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/categoryControllers/CreateCategoryController";
+import { DeleteCategoryController } from "./controllers/categoryControllers/DeleteCategoryController";
+import { GetAllCategoriesController } from "./controllers/categoryControllers/GetAllCategoriesController";
+import { UpdateCategoryController } from "./controllers/categoryControllers/UpdateCategoryController";
 
 
 const routes = Router();
 
 routes.post("/categories", new CreateCategoryController().handle);
+routes.get("/categories", new GetAllCategoriesController().handle);
+routes.delete("/categories/:id", new DeleteCategoryController().handle);
+routes.put("/categories/:id", new UpdateCategoryController().handle);
 
 export { routes };
